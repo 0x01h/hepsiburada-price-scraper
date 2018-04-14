49126da189aabacd438806a1392e3a13b8840742
Orçun Özdemir <o540az@gmail.com>
2018-04-14 20:10:44 +0300

    Add exception handling

--- hb-scraper.py
+++ hb-scraper.py
@@ -4,10 +4,13 @@ import requests
 from bs4 import BeautifulSoup
 
 productPage = input('Enter the product page URL to get price: ')
-page = requests.get(productPage)
-soup = BeautifulSoup(page.content, 'html.parser')
-parent = soup.find(id="offering-price")
-beforePoint = parent.select("span")[0]
-afterPoint = parent.select("span")[1]
 
-print(beforePoint.get_text() + '.' + afterPoint.get_text() + ' TL')
+try:
+    page = requests.get(productPage)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    parent = soup.find(id="offering-price")
+    beforePoint = parent.select("span")[0]
+    afterPoint = parent.select("span")[1]
+    print(beforePoint.get_text() + '.' + afterPoint.get_text() + ' TL')
+except:
+    print("Price couldn't be scraped!")
