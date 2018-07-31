246b97557476634ed4743d8a707cc0589c9c6e24
$ whoami <benorcunozdemir@gmail.com>
2018-07-31 16:23:06 +0300

    Add __main__ == '__main__'

--- hb-scraper.py
+++ hb-scraper.py
@@ -1,16 +1,22 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import requests
 from bs4 import BeautifulSoup
 
-productPage = input('Enter the product page URL to get price: ')
+def main():
+    productPage = input('Enter the product page URL to get price: ')
 
-try:
-    page = requests.get(productPage)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    parent = soup.find(id="offering-price")
-    beforePoint = parent.select("span")[0]
-    afterPoint = parent.select("span")[1]
-    print(beforePoint.get_text() + '.' + afterPoint.get_text() + ' TL')
-except:
-    print("Price couldn't be scraped!")
+    try:
+        page = requests.get(productPage)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        parent = soup.find(id='offering-price')
+        beforePoint = parent.select('span')[0]
+        afterPoint = parent.select('span')[1]
+        print (beforePoint.get_text() + '.' + afterPoint.get_text() + ' TL')
+    except:
+        print ("Price couldn't be scraped!")
+
+
+if __name__ == '__main__':
+    main()
